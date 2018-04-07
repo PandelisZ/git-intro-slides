71db871ae84cd08b69e8933fd0d355eb8fa4182f
Pandelis Zembashis <pandelis@d4software.co.uk>
2018-04-07 19:55:39 +0100

    Fix a glitch with reveal.js and webpack

--- src/script.js
+++ src/script.js
@@ -5,15 +5,12 @@ import 'bulma/css/bulma.css'
 import './lib/padding.css'
 import './style.css'
 import 'headjs/dist/1.0.0/head.min'
-import * as Reveal from 'reveal.js'
 
-window.Reveal = Reveal
-
-Reveal.initialize({
+window.Reveal.initialize({
   history: true,
   dependencies: [
     // d3.js library and plugin
-    { src: 'https://d3js.org/d3.v3.min.js' },
+    { src: 'https://d3js.org/d3.v4.min.js' },
     { src: '/public/d3js.js' }
   ]
 })
